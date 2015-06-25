b502e3f0b8fc8ee055ed88aa7f2922cac71fd00f
cmkarungi <cmkarungi@gmail.com>
2015-06-25 09:47:51 +0300

    added index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Passion</title>
+</head>
+<body>
+
+<h3> Just links  </h3>
+<a href="get_categories.php"> Categories </a> <br>
+<a href="new_category.php"> Post Category </a>
+
+</body>
+</html>
